9e9e246b9a9ad6de924941a087b8a9415ae1f590
FKRF <153143103+FKRF@users.noreply.github.com>
2024-05-27 01:11:51 -0300

    1827

---
+++ CSharp/Beginner/1827/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string eof;
+        int n, i, j, nCentral;
+        bool inner;
+        StringBuilder myMatrix = new StringBuilder();
+        while( (eof = Console.ReadLine()) != null )
+        {
+            n = Convert.ToInt32(eof);
+            
+            nCentral = n/2;
+            myMatrix.Clear();
+            for(i = 0; i < n; i++)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    inner = (i >= n/3) && j >= n/3 && i < n - n/3 && j < n - n/3;
+                    if(i == nCentral && j == nCentral)
+                        myMatrix.Append("4");
+                    else if(inner == true)
+                        myMatrix.Append("1");
+                    else if(i == j)
+                        myMatrix.Append("2");    
+                    else if ( i + j == n -1)
+                        myMatrix.Append("3");
+                    else
+                        myMatrix.Append("0");
+                    
+                        
+                }
+                myMatrix.Append("\n");
+            }
+            Console.WriteLine(myMatrix);
+
+        }
+    }
+
+    
+}

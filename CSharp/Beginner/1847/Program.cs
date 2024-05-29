cb85a94e6f7843d7e822a7c50ba3ce3af1ec36f4
FKRF <153143103+FKRF@users.noreply.github.com>
2024-05-29 02:09:08 -0300

    1847

--- CSharp/Beginner/1847/Program.cs
+++ CSharp/Beginner/1847/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace _1847;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] line = Console.ReadLine().Split(' ');
+        int ta, tb, tc, tab, tbc;
+
+        ta = Convert.ToInt32(line[0]);
+        tb = Convert.ToInt32(line[1]);
+        tc = Convert.ToInt32(line[2]);
+
+        tab = tb - ta;
+        tbc = tc - tb;
+
+        string answer = (tbc > tab || (tab > 0 && tbc >= tab)) ? ":)" : ":(";
+        Console.WriteLine(answer);
+        
     }
+
 }
